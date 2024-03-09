505065cc963fc82237e68a75cfe87a7190f0cd0c
Lain0x0 <lainyamato03@gmail.com>
2024-03-09 01:19:35 +0100

    zombi.exe

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
-#include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * infinite_while - Infinite loop (void).
+ * infinite_while - Infinite loop (void)
  *
  * Return: Always 0
  */
-
 int infinite_while(void)
 {
 	while (1)
@@ -17,29 +17,24 @@ int infinite_while(void)
 }
 
 /**
- * main - Creates 5 zombie processes
+ * main - Entry Point
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-	int i;
-	pid_t pid;
+	int i = 0, pid;
 
 	for (i = 0; i < 5; i++)
 	{
 		pid = fork();
-
 		if (pid > 0)
-		{
-			fprintf(stdout, "Zombie process created, PID: %d\n", pid);
-		}
-		else if (pid == 0)
-		{
-			infinite_while();
-		}
+			printf("Zombie process created, PID: %d\n", pid);
+		else
+			return (0);
 	}
+
+	infinite_while();
 
 	return (0);
 }
