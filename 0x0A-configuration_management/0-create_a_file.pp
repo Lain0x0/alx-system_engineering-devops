e1476128a1e87e73a49a6a49e566665dc6730a14
Lain0x0 <lainyamato03@gmail.com>
2024-05-03 18:43:20 +0100

    0x0A

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # creating a file in /tmp directory
 
-    file { 'school':
-    ensure  => 'present',
-    path    => '/tmp/school'
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet'
-    }
-
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
