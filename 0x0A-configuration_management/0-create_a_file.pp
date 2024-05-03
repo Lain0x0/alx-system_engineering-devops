83d66e24cfc9fc18d0f4c085883defe81a1cd249
Lain0x0 <lainyamato03@gmail.com>
2024-05-03 18:12:55 +0100

    0x0A

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creating a file in /tmp directory giving those requirements [path, permession, owner, group]
 # also this puppet file contain 'I love Puppet as content'
 
-file { '/tmp/school':
-    ensure  => 'directory',
+file { 'school':
+    ensure  => 'present',
+    path    => '/tmp/school'
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
