9da0c1db5a4d22026d54494f5741823bc103f8e6
Lain0x0 <lainyamato03@gmail.com>
2024-06-18 22:23:07 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ def number_of_subscribers(subreddit):
     headers = {
             "User-Agent":"Mozilla/5.0 (Linux; U; Linux i643 x86_64) Gecko/20100101 Firefox/50.0"
     }
-    respond = requests.get(url, headers=headers, allow_redirects=False)
-    if (respond.status_code == 404):
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    results = respond.json().get("data")
+    results = response.json().get("data")
     return results.get("subscribers")
