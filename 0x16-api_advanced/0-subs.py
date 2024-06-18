459acd48eecf67d3622e7590dea234fbb97c9e7e
Lain0x0 <lainyamato03@gmail.com>
2024-06-18 22:25:06 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ def number_of_subscribers(subreddit):
     """ set up the functionality and logic of code """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent":"Mozilla/5.0 (Linux; U; Linux i643 x86_64) Gecko/20100101 Firefox/50.0"
-    }
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
